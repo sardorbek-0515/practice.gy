66d144ae228ea7616d049aee42366d954f928dab
sardorbek-0515 <tursunovsardorbek76@gmail.com>
2026-05-06 23:23:39 +0900

    feat: python system veriable & special methods via dunders

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
-a = 100
-print("a:", a)
-message = "hello world"
+# Dunder __builtins__, __init__
+message = "Python: HAR DOIM OBJECT!"
 print(message)
+
+result = type(message)
+print("result:", result)
+
+'''In Python, there are builtin tools:
+(1)TYPES > init float str list dict
+(2)FUNCTION > print() len() input() type() str() init()
+(3)CONSTANTS > Tru False None
+'''
+
+print(dir(__builtins__))
