de9271d143842afcb0c994da78ef0d1da3baad2a
sardorbek-0515 <tursunovsardorbek76@gmail.com>
2026-05-07 01:07:44 +0900

    feat: learn string in python

--- primative.py
+++ primative.py
@@ -1,4 +1,4 @@
-print("====NUMBER===")
+print("=========NUMBER=======")
 # in JAVA veriable is a name storage location!
 # JAVA vs S languechlarida malumot manzilini nomlanishi
 # in Python,veriable is named referanse!
@@ -13,3 +13,24 @@ print(f"the count: {count} and type: {count_type}")
 result1 = count.bit_count()  # method
 result2 = count.numerator   # state
 print(result1, result2)
+
+
+print("========= String ========")
+# METHODS: upper() lower() title() find() replace()
+
+course = "AI Python FullStack"
+result = type(course)
+print(f"the result (1): {result}")
+
+
+result = course.title()
+print(f"the result (2): {result}")
+
+result = course.upper()  # kotta harf bn yozadi
+print(f"the result (3): {result}")
+
+result = course.replace("FullStack", "MasterClass")
+print(f"the result (4): {result}")
+
+
+print("========= boolean ========")
