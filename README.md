9707bb9cff7d8b91088ecc520fb93aea1b720828
sardorbek-0515 <tursunovsardorbek76@gmail.com>
2026-05-14 22:42:55 +0900

    feat: For vs While  Loop operatorlari

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# practice.gy
+<!-- # PRACTICE PROJECT
+Learning Git and Python Function 
+## GIT COMMANDS
+
+```
+git config --global user.mame
+git config --global user.name "your_name
+git config --global user.email "your_email@example.com"
+
+git status
+git add .
+git commit -m "feat: Initial Setup"
+
+git remote add origin https;//githup_adres
+git push origin master
+git pull origin master
+
+```
+
+## Python Foundation:
+
+- Primitive Variables
+- Function
+- Object
+- Class about
+- Class deep dive > Encapsulation
+- Class deep dive > Inheritence Polimorphism
+- Operation & Conditions
+- Loops
+- Tuple
+- List
+- Array
+- Comprehension
+- Debugging
+- ....
+
+## PYTHONda xammanarsa OBJECT bo'ladi
+
+## pythonning ishlashi DUNDER VERIABLE yoki MAXSUS METHODlarga asoslanadi
+
+# PROPERTY
+
+- method() xamda state -->
---
+++ loops.py
@@ -0,0 +1,66 @@
+'''   LOOP operators
+      (1) for 
+      (2) break/else
+      (3)while
+'''
+print("========== (1) for operator============")  # {ketmaketlik aniq boladi}
+# For - belgilangan tartibda, belgilangan ketmaketlikda ishga tushishi nazarda ttilgan mantiqlar uchun ishlatiladi
+# Iterable object > string dict tuple list range map filter / takrorlanish xususiyatga ega object
+text = "MIT"  # 3maarta loop boladi
+numbs = [10, 7, 3, 4]
+car_obj = dict(brand="Ferarri", year=2025)  # 2marta taakrorlanadi
+range_obj = range(5)  # [0, 5]
+
+for letter in text:  # loop qilib beradi
+    print(f"the letter: {letter}")
+
+print("------")
+
+for number in numbs:
+    print(f"the number: {number}")
+
+print("----------")
+for x in range_obj:
+    print(f"the element: {x}")
+
+print("----keylarni qolga kiritsh---")
+for key in car_obj:
+    print(f"the key: {key}")
+
+
+print("----keylar & value qolga kiritsh---")
+for key in car_obj:
+    print(f"the key: {key} => value: {car_obj.get(key)}")
+
+print("----------")
+for x in range(1, 20, 5):
+    print(f"the x: {x}")
+
+
+print("========== (2) break/else ============")
+for x in range(1, 20, 5):
+    print(f"the x: {x}")
+    if x > 10:
+        print("Tanafusga chiqdik")
+        break  # break majburiy toxtatish
+else:                # break bolmasa else amalga oshadi
+    print("Lopp amali hech qanday xatoliksz amalga oshdi ")
+
+
+print("==========  (3) while  operator =========")  # ketmaketlik noaniq boladi
+numb = 40
+while numb > 0:  # shart
+    numb -= 10  # har ishga tushganda 10ga kamayadi
+    print(f"raqam teng {numb}")
+
+print("----------")
+count = 0
+while True:
+    count += 1
+    x = int(input("Raqamni toping"))
+
+    if x == 41:
+        print(f"You found number in {count} steps")
+        break
+    else:
+        print("Wrong, please find again")
